fc39d281ff6fc126f58b4b6fdb360c3650edb188
Mack <macknz7@gmail.com>
2020-09-08 14:41:28 -0700

    Add project files.

---
+++ csharpExploration/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpExploration
+{
+    class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Person(string first, string last)
+        {
+            FirstName = first;
+            LastName = last;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public string AllCaps()
+        {
+            return ToString().ToUpper();
+        }
+    }
+}
---
+++ csharpExploration/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharpExploration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person("Bill", "Wagner");
+            Console.WriteLine("The name, in all caps: " + p.AllCaps());
+            Console.WriteLine("The name: " + p);
+        }
+    }
+}
